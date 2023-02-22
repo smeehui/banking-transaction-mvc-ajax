a13d323aaf84577d038adc720699820ebef8e3ef
smeenguyen <nguyenvanquanghuy99@gmail.com>
2023-02-23 00:59:30 +0700

    Delete user

---
+++ src/main/java/com/cg/config/DependenciesBeans.java
@@ -0,0 +1,13 @@
+package com.cg.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DependenciesBeans {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
